12f18fa153db76594dffd5d2af9a1e77baa8ce37
Mark Pedersen <markpedersen@MacBook-Pro.local>
2020-06-21 22:44:52 -0700

    First commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// The load balancer should run a health check on each server periodically.
+fn health_check() {}
+
+// Config file should contain the list of server addresses.
+fn parse_config_file() {}
+
+fn main() {}
