f37b5f5c9a50418de9776cf2d3ef740e91b6b097
Mark Pedersen <markpedersen@MacBook-Pro.local>
2020-06-21 22:55:37 -0700

    Template

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
+use tokio::net::TcpListener;
+use tokio::{prelude::*, spawn};
+
 // The load balancer should run a health check on each server periodically.
 fn health_check() {}
 
 // Config file should contain the list of server addresses.
 fn parse_config_file() {}
 
-fn main() {}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
